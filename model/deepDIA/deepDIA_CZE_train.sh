13cdf07ff883ddf296bdea8ea67f2ffd8201379f
wenrongchen <43869196+wenronchen@users.noreply.github.com>
2022-02-14 09:33:49 -0600

    Update deepDIA_CZE_train.sh

--- model/deepDIA/deepDIA_CZE_train.sh
+++ model/deepDIA/deepDIA_CZE_train.sh
@@ -14,7 +14,7 @@ do
 		do
 			for kernel_size in 3
 			do
-				python3 train_model_split.py $1 $2_$cnt $max_seq $max_rt $min_rt $filter_size $kernel_size $lstm_feature $dropout_rate $dense_feature $y_attr
+				python3 train_model_split.py $1 $2_$cnt $max_seq $max_rt $min_rt $filter_size $kernel_size $lstm_feature $dropout_rate $dense_feature "$y_attr"
 				echo "$cnt training complete"
 				cnt=$((cnt+1))
 				echo filter_size=$filter_size, kernel_size=$kernel_size, LSTM_Feature=$lstm_feature, dropout_rate=$dropout_rate, dense_feature=$dense_feature, max_seq=$max_seq, max_rt=$max_rt
